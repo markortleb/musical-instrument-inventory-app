f703a5bd4ac847a2731409577c98a6b0f9d26f2c
Mark Ortleb <markortleb@gmail.com>
2023-07-03 13:47:00 -0400

    adding function to get conn string

--- app.ts
+++ app.ts
@@ -1,15 +1,39 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import * as express from "express";
 import * as indexRouter from './routes/index';
 
 
-const app: express.Express = express();
+function getMongoConnString(): string {
+    interface ConnInfo {
+        username: string,
+        password: string,
+        hostname: string
+    }
 
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
+    const dbConnInfo: ConnInfo = JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../db_conn_info/db_conn_info.json'), 'utf-8')
+    );
 
-app.use(express.static(path.join(__dirname, './public')));
+    return `mongodb+srv://${dbConnInfo.username}:${dbConnInfo.password}@${dbConnInfo.hostname}`;
+}
 
-app.use('/', indexRouter);
+
+function createApp(): express.Express {
+    const newApp: express.Express = express();
+
+    newApp.set('views', path.join(__dirname, '../views'));
+    newApp.set('view engine', 'pug');
+
+    newApp.use(express.static(path.join(__dirname, './public')));
+
+    newApp.use('/', indexRouter);
+
+    return newApp;
+}
+
+
+const app: express.Express = createApp();
+
 
 export = app;
